b4d7f93ff84d634ddf7f4ecdf4bae6e7b900d174
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-23 23:16:11 +0530

    Initial upload

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdowns = document.querySelectorAll(".dropdown");
+
+    dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener("click", function(event) {
+            event.stopPropagation();
+            
+            dropdowns.forEach((d)=> {
+                if(d !== dropdown){
+                    d.querySelector(".dropdown-list").style.display = "none";
+                }
+            });
+
+            const dropdownList = dropdown.querySelector(".dropdown-list");
+
+            if(dropdownList.style.display === "block"){
+                dropdownList.style.display = "none";
+            } else {
+                dropdownList.style.display = "block";
+            }
+        });
+    });
+
+    document.addEventListener("click", function() {
+        dropdowns.forEach((dropdown) => {
+            dropdown.querySelector('.dropdown-list').style.display = 'none';
+        })
+    })
+})
