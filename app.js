e591a17c9a2a649a99e1d558ab7dfb5b540f1bf0
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:04:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const dropdowns = document.querySelectorAll(".dropdown");
 
     dropdowns.forEach((dropdown) => {
+        // Add click event listener to each dropdown
         dropdown.addEventListener("click", function(event) {
-            event.stopPropagation();
+            event.stopPropagation(); // Prevent click events from bubbling up
 
-            dropdowns.forEach((d)=> {
-                if(d !== dropdown){
+            // Close other open dropdowns
+            dropdowns.forEach((d) => {
+                if (d !== dropdown) {
                     d.querySelector(".dropdown-list").style.display = "none";
                 }
             });
 
+            // Toggle the display of the current dropdown's list
             const dropdownList = dropdown.querySelector(".dropdown-list");
-
-            if(dropdownList.style.display === "block"){
+            if (dropdownList.style.display === "block") {
                 dropdownList.style.display = "none";
             } else {
                 dropdownList.style.display = "block";
@@ -21,9 +23,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // Close all dropdowns when clicking outside of them
     document.addEventListener("click", function() {
         dropdowns.forEach((dropdown) => {
             dropdown.querySelector('.dropdown-list').style.display = 'none';
-        })
-    })
-})
+        });
+    });
+});
